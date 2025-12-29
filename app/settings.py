e7f5c95b88600222700c9a0683cb05adae17eca0
Abhiroop <abhiroop1998.dev@gmail.com>
2025-12-29 13:59:52 +0530

    feat: update CORS settings to use regex for allowed origins

--- app/settings.py
+++ app/settings.py
@@ -79,12 +79,8 @@ MIDDLEWARE = [
 ]
 
 # CORS Configurations
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:9000",
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
 ]
 
 ROOT_URLCONF = 'app.urls'
